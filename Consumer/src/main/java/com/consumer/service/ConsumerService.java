00ec0da570f9951c575e85360ee2e40a2ed62862
Reshma Thomas <reshma@dhcp-v051-022.mobile.uci.edu>
2022-06-05 11:24:54 -0700

    Set up postgres to save notifications TODO: Test crud operations on the db

--- Consumer/src/main/java/com/consumer/service/ConsumerService.java
+++ Consumer/src/main/java/com/consumer/service/ConsumerService.java
@@ -1,31 +1,35 @@
 package com.consumer.service;
 
 
-import com.consumer.model.Mail;
+import com.consumer.domain.RSSItem;
+import com.consumer.domain.RSSItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.event.EventListener;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.kafka.event.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
-import org.springframework.core.io.*;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.net.*;
-import java.io.*;
-import javax.mail.*;
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
 import java.util.stream.Collectors;
-import java.nio.charset.*;
 
 @Service
 public class ConsumerService {
     // To log the received messages
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerService.class);
+
+    @Autowired
+    private RSSItemRepository rssItemRepository;
 
     @Value("${kafka.consumer.group1.topic}")
     private String group1Topic;
@@ -79,18 +83,13 @@ public class ConsumerService {
 
     private void consume(String data, String topic) {
         LOGGER.info("Received message='{}'", topic + ": " + data);
-        Mail mail = new Mail();
-        // TODO: Replace with config read from application.yaml
-        mail.setMailFrom("cs237uci@gmail.com");
-        // Get the set of subscribers for this topic
-        List<String> subsList = new ArrayList<String>(subscriptions.get(topic));
-        //mail.setMailTo("cs237uci@gmail.com");
-        //List<String> subsList = new ArrayList<String>();
-        //subsList.add("cs237uci@gmail.com");
-        mail.setMailTo(subsList);
-        mail.setMailSubject(topic + " Notification");
-        mail.setMailContent(data);
-        mailService.sendEmail(mail, session, transport);
+        // TODO: Save as string only, but add timestamp
+        rssItemRepository.save(
+                RSSItem.builder()
+                        .topic(topic)
+                        .data(data)
+                        .build()
+        );
     }
 
     private void setUpSubscriptions() throws Exception {
@@ -121,41 +120,18 @@ public class ConsumerService {
     }
 
     private void setUpTransport() throws Exception {
-        LOGGER.debug("In setup transport phase");
-        Properties props = new Properties();
-        props.put("mail.smtp.host", smtpHost); //SMTP Host
-        props.put("mail.smtp.port", smtpPort); //TLS Port
-        props.put("mail.smtp.auth", smtpAuth); //enable authentication
-        props.put("mail.smtp.starttls.enable", smtpAuth); //enable STARTTLS
-        props.put("mail.smtp.ssl.trust", "smtp.gmail.com"); //enable STARTTLS
-        final String fromEmail = smtpUsername + "@gmail.com";
-        Authenticator auth = new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(fromEmail, smtpPassword);
-            }
-        };
-        session = Session.getInstance(props, auth);
-        //session.setDebug(true);
-        try {
-            LOGGER.debug("Creating transport for the consumer service" + " " + fromEmail);
-            transport = session.getTransport("smtp");
-            LOGGER.debug("Creating transport connection for the consumer service "+ smtpHost + " " + smtpUsername + " " + smtpPassword);
-            transport.connect(smtpHost, 587, smtpUsername, smtpPassword);
-        } catch(Exception e)
-        {
-            LOGGER.error("Transport creation OR connection failed");
-            e.printStackTrace();
-        }
+        // might need to replace with slack
     }
 
     //@EventListener(ConsumerStartedEvent.class)
     @EventListener(ApplicationStartedEvent.class)
     public void setUp() throws Exception {
         LOGGER.debug("In setup phase");
+        //rssItemRepository = new RSSItemRepository();
         // 1. Create subscription list
-        setUpSubscriptions();
+        //setUpSubscriptions();
         // 2. Set up transport to send mails to the subscribers
-        setUpTransport();
+        //setUpTransport();
     }
 
     //@EventListener(ConsumerStoppedEvent.class)
